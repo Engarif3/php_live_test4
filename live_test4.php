f5960dfc220ce5a6f8f7ddfd33ecf8b45a583ba1
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-10-04 19:10:09 +0200

    live test 4 task 2 done

--- live_test4.php
+++ live_test4.php
@@ -21,7 +21,36 @@ class Person {
 $person = new Person("John", 30);
 $person->introduce();
 
+echo "<br>";
+echo "<br>";
+
 ?>
 
 <!-- Task 2: -->
 
+<?php
+class Student extends Person {
+
+    public $mark;
+
+    public function __construct($name, $age, $mark) {
+        parent::__construct($name, $age);
+        $this->mark = $mark;
+    }
+    
+    public function introduce() {
+        echo "My name is {$this->name}, I am {$this->age} years old.<br>";
+    }
+
+    public function calculate_grade_percentage() {
+        return "{$this->mark}%";
+    }
+}
+
+// Example usage
+$student = new Student("Alice", 18, "85");
+$student->introduce();
+$gradePercentage = $student->calculate_grade_percentage();
+echo "My grade percentage is {$gradePercentage}\n";
+
+?>
